6da17dc9a5359a4b50096ca9b6686958cfc619be
ramondorosario <ramondorosario@gmail.com>
2021-06-22 00:52:53 -0300

    chore: use header data from api

--- src/types/api.ts
+++ src/types/api.ts
@@ -3,6 +3,20 @@ export type LogoProps = {
   alternativeText: string
 }
 
+export type HeaderProps = {
+  title: string
+  description: string
+  button: {
+    label: string
+    url: string
+  }
+  image: {
+    alternativeText: string
+    url: string
+  }
+}
+
 export type LandingPageProps = {
   logo: LogoProps
+  header: HeaderProps
 }
