76c6a73cb7b7054708833eed6bdd6ce3d96e3d4e
ramondorosario <ramondorosario@gmail.com>
2021-06-24 23:02:06 -0300

    chore: remove file

--- src/components/SectionAboutUs/index.tsx
+++ src/components/SectionAboutUs/index.tsx
@@ -4,9 +4,9 @@ import Heading from 'components/Heading'
 import Container from 'components/Container'
 import ProfileCard from 'components/ProfileCard'
 
-import content from './content'
-import * as S from './styles'
 import { SectionAboutUsProps } from 'types/api'
+
+import * as S from './styles'
 
 const SectionAboutUs = ({ title, authors }: SectionAboutUsProps) => (
   <Container>
